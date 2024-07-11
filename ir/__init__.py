5318b9e0e74dc21274f48a120926d5145b159d16
CKwin26 <156837805+CKwin26@users.noreply.github.com>
2024-07-11 13:02:44 -0400

    Create __init__.py

---
+++ ir/__init__.py
@@ -0,0 +1,13 @@
+# ir/__init__.py
+
+# 导入包中的模块
+from . import ORB
+from . import draw
+from . import manual
+from . import manual_org
+from . import manual_select_ORB
+from . import match_contour
+from . import selectfront
+
+# 定义包的版本
+__version__ = "0.1"
