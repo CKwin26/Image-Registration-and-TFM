3c6060a19c37eb80529d6a3d7bb9d95edd844937
CKwin26 <156837805+CKwin26@users.noreply.github.com>
2024-07-11 13:07:31 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,20 @@ from setuptools import setup, find_packages
 setup(
     name='ir',
     version='0.1',
-    packages=find_packages(),  # 自动查找和包括所有包
+    packages=find_packages(),
     install_requires=[
-        # 依赖项列表
+        'numpy>=1.16.2',
+        'matplotlib>=3.0',
+        'pandas>=1.1.5',
+        'scipy>=1.7.0',
+        'scikit-image>=0.14.2',
+        'openpiv>=0.23.4',
+        'tqdm>=4.26.0',
+        'natsort>=5.1.0',
+        'dill>=0.2.9',
+        'roipoly>=0.5.2',
+        'openpyxl',
+        'pyyaml',
+        'matplotlib-scalebar'
     ],
 )
-
