2480c13fe3d5e02804cf8f39349ad40c8ec42aa1
CKwin26 <156837805+CKwin26@users.noreply.github.com>
2024-07-20 11:44:46 -0400

    Update auto_select.py

--- ir/auto_select.py
+++ ir/auto_select.py
@@ -1,6 +1,7 @@
 import imageio
 import pandas as pd
 import os
+import numpy as np
 from skimage.filters import threshold_otsu, gaussian
 from skimage.morphology import binary_closing, disk, remove_small_objects, label
 from scipy.ndimage.measurements import center_of_mass
